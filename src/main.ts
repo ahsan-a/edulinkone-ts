0b0df8142a9e5426cb28cc36fc83abd08b8c39be
Ahsan Ahmed <50714675+TheTechnoGamer01@users.noreply.github.com>
2021-04-12 21:11:34 +0100

    added basic start menu

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
 
 import state from './state';
-state;
+import menu from './menu';
+
+if (Object.keys(state.credentials).length < 1 || Object.keys(state.school).length < 1) {
+	console.log('work');
+	menu.login.init();
+} else {
+	console.log('not work');
+}
+
+//how come no node_module folder
+//should be in root
--- src/state/index.ts
+++ src/state/index.ts
@@ -7,13 +7,15 @@ try {
 	state.school = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../config/school.json'), 'utf8'));
 } catch {
 	fs.writeFileSync(path.resolve(__dirname, '../config/school.json'), '{}');
-	state.school = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../config/school.json'), 'utf8'));
+	state.school = {};
 }
 try {
 	state.credentials = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../config/credentials.json'), 'utf8'));
 } catch {
 	fs.writeFileSync(path.resolve(__dirname, '../config/credentials.json'), '{}');
-	state.credentials = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../config/credentials.json'), 'utf8'));
+	state.credentials = {};
 }
 
 export default state;
+
+// there's a folder called src/config which i'm reading for our config, i just copy/paste from chrome devtools for the api stuff
