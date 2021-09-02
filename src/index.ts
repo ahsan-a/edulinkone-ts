2fea31c5cf97c5c44c62ce1165d066406aa63f06
Ahsan Ahmed <50714675+TheTechnoGamer01@users.noreply.github.com>
2021-09-02 16:22:07 +0100

    added shebang

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
+#!/usr/bin/env node
 import state from './state';
 import menu from './menu';
 
-if (
-	!(Object.keys(state.credentials).length && Object.keys(state.school).length)
-) {
+if (!(Object.keys(state.credentials).length && Object.keys(state.school).length)) {
 	menu.login.init();
 } else {
 	menu.main.mainMenu();
